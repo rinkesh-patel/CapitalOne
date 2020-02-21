7120634b246a2b8877fcd8515ed38cecdfdc709a
Patel <patelrinkeshm@gmail.com>
2020-02-20 20:35:48 -0600

    Init

---
+++ src/main/java/com/CapitalOne/OrganizationData/OrganizationDataApplication.java
@@ -0,0 +1,13 @@
+package com.CapitalOne.OrganizationData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizationDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizationDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CapitalOne/OrganizationData/OrganizationDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CapitalOne.OrganizationData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizationDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
